05bcf7c4cba630b8bc2f267c7bfc09c5cafcac70
olzhas <o.malikov@p-s.kz>
2020-11-03 13:29:36 +0600

    numberRevert solved

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -125,4 +125,12 @@ fun accountInThreeYears(initial: Int, percent: Int): Double = TODO()
  * Пользователь задает целое трехзначное число (например, 478).
  * Необходимо вывести число, полученное из заданного перестановкой цифр в обратном порядке (например, 874).
  */
-fun numberRevert(number: Int): Int = TODO()
+fun numberRevert(number: Int): Int {
+    var rev = 0
+    var num = number
+    while (num > 0) {
+        rev = (rev * 10) + (num % 10)
+        num /= 10
+    }
+    return rev
+}
